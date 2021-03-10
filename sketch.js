837de9f61d3e15186fed9d4da728aa5d73ad7b9b
Raman707547 <77740276+Raman707547@users.noreply.github.com>
2021-03-10 20:57:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var  database;
+var canvas,playerCount,form,player,game;
+var gameState=0;
+
+function setup(){
+  database = firebase.database();
+  
+  createCanvas(500,500);
+
+}
+
+function draw(){
+  background("white");
+  
+   
+  
+}
+
